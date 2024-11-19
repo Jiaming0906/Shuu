de0e13d93232312d2e07ed0c8c969421304de517
Yu Jiaming <yuey9012@gmail.com>
2024-11-19 16:38:51 +0800

    changes

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-### Discord Bot created with Discord.js API
-
-First create *.env* file in your folder in the format:
-
-DISCORD_TOKEN=123456789
-CLIENT_ID=123456789
-
-Please change "123456789" accordingly.
-
-1. Run `node .\deploy-commands.js`
-2. Run `node .\index.js`
-
-Bot should be online.
+### for personal use
--- index.js
+++ index.js
@@ -49,28 +49,6 @@ for (const file of commandFiles){
 };
 
 //welcome message
-const welcomeChannelId = "1176061415711318056";
-
-client.on(Events.GuildMemberAdd, async (member) => {
-    try {
-        const a = ["H", "O", "W", "L", "h", "o", "w", "l", "A", "I", "R", "a", "i", "r", "."];
-        if (a.includes(member.user.username[0])) {
-
-            await client.channels.cache.get(welcomeChannelId).send(`Welcome to Laputa <@${member.id}>! Please read through the match-rules https://discord.com/channels/1176055204358139964/1176065449297981500 and collect your roles https://discord.com/channels/1176055204358139964/1176065391622115398 here. The sunset's breathtaking here, don't forget to catch it!`);
-
-            const roleId = "1176056312300326940";
-            let roleToAdd = await member.guild.roles.cache.get(roleId);
-            await member.roles.add(roleToAdd);
-
-        };
-
-    } catch (err) {
-        console.log("index.js, guildmemberadd, welcome message");
-        console.log("error message below".padEnd(50, "-"));
-        console.log(err)
-        console.log("-".padEnd(50, "-"));
-    };
-});
 
 //commands interaction
 client.once(Events.ClientReady, () => {
@@ -87,6 +65,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
     try {
         await command.execute(interaction);
+
     } catch (err){
         console.log("index.js, client.on Events.InteractionCreate");
         console.log("err message below".padEnd(50, "-"));
@@ -102,222 +81,11 @@ client.on(Events.InteractionCreate, async interaction => {
 //DM bot
 
 //bot message channel
-const botMessageChannel = "1176068732599160902";
-const botUserId = "<@1177117634207158312>";
 
 //recieve dm message reports
-client.on("messageCreate", async (message) => {
-
-    //ignores if message was sent to guild
-    if(message.guild) return;
-    if(message.author.bot) return;
-
-    try {
-        //
-        message.react("💟");
-
-        //create embed
-        const embed = new EmbedBuilder()
-            .setColor("#8E7AF0")
-            .setDescription(`From DM, <@${message.author.id}>:\n${message.content}`);
-            
-        //if got picture image in message
-        // if (message.attachments.first()) {
-        //     //console.log(message.attachments.first());
-        //     await client.channels.cache.get(botMessageChannel).send(`From DM, ${message.author.username}:\n${message.content} (with photo attached)`);
-        //     return;
-        // };
-
-        await client.channels.cache.get(botMessageChannel).send({ embeds: [embed] });
-        return;
-
-    } catch (err) {
-        console.log("index.js, receive DM Message Error");
-        console.log("err message below".padEnd(50, "-"));
-        console.log(err);
-        console.log("-".padEnd(50, "-"));
-    };
-});
-
-//react to ready message
-client.on("messageCreate", async (message) => {
-    if (message.author.system) return;
-    if (message.author.bot) return;
-
-    if (message.content.startsWith("Ready") || message.content.startsWith("ready")) {
-        message.react("☀️");
-        return;
-    };
-});
-
-//react to ping
-
-client.on("messageCreate", async (message) => {
-    
-    if(message.author.bot) return;
-    if(message.author.system) return;
-    if(message.author.id === "697822391971086406") return;
-
-    //console.log(message);
-
-    try {
-        //
-        if(message.content.includes(botUserId)){
-            message.react("💟");
-            message.reply("Message received! I will get back to you soon!");
-
-            //create an embed 
-            const embed = new EmbedBuilder()
-            .setColor("#FFBB91")
-            .setDescription(`From Category _${message.channel.parent.name}_ > Channel _${message.channel.name}_, <@${message.author.id}>:\n${message.content}`);
-
-            //if got picture image in message
-            // if (message.attachments.first()) {
-            //     //console.log(message.attachments.first());
-            //     await client.channels.cache.get(botMessageChannel).send(`From ${message.channel.parent.name} > ${message.channel.name}, ${message.author.username}:\n${message.content} (with photo)`);
-            //     return;
-            // };
-    
-            //var messageLink = "https://discord.com/channels/" + String(message.guildId) + "/" + String(message.channelId) + "/" + String(message.id);
-            await client.channels.cache.get(botMessageChannel).send({ embeds: [embed] });
-            return;
-        };
-
-    } catch (err) {
-        console.log("index.js, ping error");
-        console.log("error message below".padEnd(50, "-"));
-        console.log(err);
-        console.log("-".padEnd(50, "-"));
-    };
-});
 
 //reaction add
-
-const reactionMessageId = "1164886437540405248";
-const reactionLogChannelId = "1176068410841497650";
-
-//roles to add
-const rolesObj = {"🥶": "1164885298715578398", "😵‍💫": "1164885373164470303"};
-
-client.on(Events.MessageReactionAdd, async (reaction, user) => {
-
-    if (reaction.message.id === reactionMessageId) {
-        //only triggers for reactions on reactionMessage only
-
-        if (reaction.message.partial) {
-            try {
-                await reaction.message.fetch();
-            } catch (err) {
-                console.log("index.js, add roles, message partial");
-                console.log("error message below".padEnd(50, "-"));
-                console.log(err);
-                console.log("-".padEnd(50, "-"));
-            };
-        };
-
-        // const testonly = [];
-        // reaction.message.reactions.cache.forEach(r => testonly.push(r))
-        // console.log(testonly);
-        
-        if (rolesObj[reaction._emoji.name]) {
-            try {
-                let roleToAdd = await reaction.message.guild.roles.cache.get(rolesObj[reaction._emoji.name]);
-                await reaction.message.guild.members.cache.get(user.id).roles.add(roleToAdd);
-
-                //if custom emoji 
-                if (reaction._emoji.id){
-                    await client.channels.cache.get(reactionLogChannelId).send(`${user.username} reacted with <:${reaction._emoji.name}:${reaction._emoji.id}>, role "${roleToAdd.name}" has been given.`);
-                    return;
-                };
-                await client.channels.cache.get(reactionLogChannelId).send(`${user.username} reacted with ${reaction._emoji.name}, role "${roleToAdd.name}" has been given.`);
-                return;
-
-            } catch (err) {
-                // console.log(`${user.username} faced an error trying to add role with reaction`)
-                // console.log("-".padEnd(39, "-"));
-                console.log(err);
-                console.log("-".padEnd(50, "-"));
-
-                try {
-                    //send error message to logs channel
-                    await client.channels.cache.get(reactionLogChannelId).send(`🔴 ${user.username} needs help with adding role.`);
-
-                } catch (err) {
-                    console.log("error sending error to add role message to logs channel");
-                    console.log("error message below".padEnd(50, "-"));
-                    console.log(err);
-                    console.log("-".padEnd(50, "-"));
-                }
-            }
-        }
-    };
-});
-
 // reaction remove 
-
-client.on(Events.MessageReactionRemove, async (reaction, user) => {
-
-    if (reaction.message.id === reactionMessageId) {
-        //only triggers for reactions on reactionMessage only
-
-        if (reaction.message.partial) {
-            try {
-                await reaction.message.fetch();
-            } catch (err) {
-                console.log("index.js, reaction remove, message partial");
-                console.log("error message below".padEnd(50, "-"));
-                console.log(err);
-                console.log("-".padEnd(50, "-"));
-            };
-        };
-
-        if (user.partial) {
-            try {
-                await reaction.message.guild.members.fetch(user.id);
-                // await client.channels.cache.get(reactionLogChannelId).send(`🔴 ${userCache.user.username} needs help to remove "${reaction._emoji.name}" role.`);
-                // return;
-                
-            } catch (err) {
-                console.log("index.js, reaction remove, user partial");
-                console.log("error message below".padEnd(50, "-"));
-                console.log(err);
-                console.log("-".padEnd(50, "-"));
-            };
-        };
-        
-        if (rolesObj[reaction._emoji.name]) {
-            try {
-                await reaction.message.guild.members.fetch(user.id);
-                let roleToRemove = await reaction.message.guild.roles.cache.get(rolesObj[reaction._emoji.name]);
-                await reaction.message.guild.members.cache.get(user.id).roles.remove(roleToRemove);
-
-                //if custom emoji 
-                if (reaction._emoji.id){
-                    await client.channels.cache.get(reactionLogChannelId).send(`${user.username} has removed reaction <:${reaction._emoji.name}:${reaction._emoji.id}>, role "${roleToRemove.name}" has been removed.`);
-                    return;
-                };
-                await client.channels.cache.get(reactionLogChannelId).send(`${user.username} has removed reaction ${reaction._emoji.name}, role "${roleToRemove.name}" has been removed.`);
-                return;
-
-            } catch (err) {
-                //console.log(`${user.username} faced an error in role removing with reaction removing`);
-                console.log(err);
-                console.log("-".padEnd(50, "-"));
-
-                try {
-                    //send role error message to logs channel
-                    await client.channels.cache.get(reactionLogChannelId).send(`🔴 ${user.username} needs help to remove "${reaction._emoji.name}" role.`);
-
-                } catch (err) {
-                    console.log("index.js, reaction remove");
-                    console.log("error message below".padEnd(50, "-"));
-                    console.log(err);
-                    console.log("-".padEnd(50, "-"));
-                }
-            }
-        }
-    };
-});
 
 // log bot onto discord
 
